e5c02202f6fbcc9a8bd4332a1f3b4c7c7242c147
letianw <tmac4ever0824@gmail.com>
2016-05-18 03:10:20 +1000

    closes #25 with caveats

--- spec/features/make_booking_request_spec.rb
+++ spec/features/make_booking_request_spec.rb
@@ -6,10 +6,18 @@ feature "a user can make a booking request" do
   end
 
   scenario "user can make a booking request on the detail page of a space" do
+    sign_out
+    sign_up(name:'Letian', email:'letian@yahoo.com')
     make_booking_request
     expect(BookingRequest.all.length).to eq(1)
-    expect(BookingRequest.all.first.user.name).to eq('Lexi')
+    expect(BookingRequest.all.first.user.name).to eq('Letian')
     expect(BookingRequest.all.first.space.title).to eq('Example Title')
+  end
+
+  scenario "user cannot make a booking request on his own spaces" do
+    make_booking_request
+    expect(BookingRequest.all.length).to eq(0)
+    expect(page).to have_content('You cannot book your own space!')
   end
 
 end
