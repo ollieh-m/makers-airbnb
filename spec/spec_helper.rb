371102431759b9617b6b5aff607eb6aeeb6f8802
ChrisKummelstedt <chris.kummelstedt@gmail.com>
2016-05-17 14:10:20 +0200

    databasecleaner strategy works with test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,7 +52,7 @@ RSpec.configure do |config|
   end
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with(:truncation)
   end
   config.before(:each) do
